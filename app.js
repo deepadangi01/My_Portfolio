09f1dae53a521c2306ceecb794d86981b21302f3
deepadangi01 <dangideepa1999@gmail.com>
2024-08-11 00:43:54 -1200

    designed

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ function pageTransitions(){
     for(let i =0; i<secBtn.length;i++ ){
         secBtn[i].addEventListener('click',function(){
             let currentBtn=document.querySelectorAll('.active-btn');
-            currentBtn[0].className= currentBtn[0].className.replace('active-btn','');
+            currentBtn[0].className=currentBtn[0].className.replace('active-btn','');
             this.className += 'active-btn'
         })
     }
